1cf45ecc6588320d68ebe361aec2903475b12f09
warunee dinunzio <dinunziow@gmail.com>
2022-06-22 11:11:38 -0400

    Learn about class and constructors

--- google-IT-Automation/classes.py
+++ google-IT-Automation/classes.py
@@ -5,7 +5,7 @@ class Birds:
         return num.number
 bluejay = Birds()
 bluejay.count()
-print(bluejay.count())
+print(bluejay.number)
 
 class Flower:
   color = 'unknown'
@@ -20,4 +20,50 @@ this_pun_is_for_you = Flower()
 
 print("Roses are {},".format(rose.color))
 print("violets are {},".format(violet.color))
-print(rose()) 
+
+
+# method in class
+class Dog:
+  name = "kevin"
+  years = 0
+  def speak(self):
+    print("Wof Wof! I'm a dog name {}!".format(self.name))
+  def dog_years(self):
+    return self.years * 7
+
+bob = Dog()
+bob.name = "Bob"
+bob.years = 2
+bob.speak()
+print(bob.dog_years())
+
+class Person:
+    def __init__(self, name):
+        self.name = name    
+    def greeting(self):
+        # Should return "hi, my name is " followed by the name of the Person.
+        return print("hi, my name is {}".format(self.name)) 
+
+# Create a new instance with a name of your choice
+some_person = Person("Jeab") 
+
+# Call the greeting method
+some_person.greeting()
+
+# This is example of defaut construtor
+class Cat:
+  def __init__(self):
+    self.cat = "Meaw, Meaw!"
+  def greeting(self):
+    print(self.cat)
+cuteCate = Cat()
+cuteCate.greeting()
+
+class Cats:
+  def __init__(self,name, greeting):
+    self.name = name
+    self.greeting = greeting
+  def greeting_cat(self):
+    print("Cat's name " + str(self.name) + " says '" + str(self.greeting) + "'")
+cute_cat = Cats("Garfild", "What's up?")
+cute_cat.greeting_cat()
